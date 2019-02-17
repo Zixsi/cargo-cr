c0056e0e8ffef058347c88c32374da82c1ec13f6
zixsi <zixxsi@gmail.com>
2019-02-17 14:24:39 +0000

    +

---
+++ callback.php
@@ -0,0 +1,41 @@
+<meta http-equiv='refresh' content='3; url=/'>
+<meta charset="UTF-8" />
+<?php
+include('inc/app.php');
+
+try
+{
+	$_POST = array(
+		'name' => ((isset($_POST['name']) && !empty($_POST['name']))?$_POST['name']:''),
+		'phone' => ((isset($_POST['phone']) && !empty($_POST['phone']))?$_POST['phone']:''),
+		'g-recaptcha-response' => ((isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response']))?$_POST['g-recaptcha-response']:'')
+	);
+
+	if(empty($_POST['name']) || empty($_POST['phone']))
+	{
+		throw new Exception('Вы заполнили не все обязательные поля, вернитесь назад и заполните необходимые поля!', 1);
+	}
+
+	if(!check_recaptcha())
+	{
+		throw new Exception('Защита от ботов не пройдена!', 1);
+	}
+
+	$address = 'info-cr-group@yandex.ru';
+	$sub = 'Обратный звонок';
+	$mes = "Имя: ".$_POST['name']."\n Контактный телефон: ".$_POST['phone']."\n";
+
+	if(mail($address, $sub, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:".$address))
+	{
+		throw new Exception('Сообщение отправлено успешно, через 3 секунды Вы будете направлены на главную страницу', 1);
+	}
+	else
+	{
+		throw new Exception('Ошибка, сообщение не отправлено!', 1);
+	}
+}
+catch(Exception $e)
+{
+	echo $e->getMessage();
+}
+die();
--- mail.php
+++ mail.php
@@ -1,23 +1,35 @@
-<meta http-equiv='refresh' content='3; url=http://www.cargo-cr.ru'>
+<meta http-equiv='refresh' content='3; url=/contact.html'>
 <meta charset="UTF-8" />
 <?php
+include('inc/app.php');
+
 if (isset($_POST['name'])) {$name = $_POST['name']; if ($name == '') {unset($name);}}
 if (isset($_POST['email'])) {$email = $_POST['email']; if ($email == '') {unset($email);}}
 if (isset($_POST['sub'])) {$sub = $_POST['sub']; if ($sub == '') {unset($sub);}}
 if (isset($_POST['body'])) {$body = $_POST['body']; if ($body == '') {unset($body);}}
  
-if (isset($name) && isset($email) && isset($sub) && isset($body)){
- 
-$address = "info-cr-group@yandex.ru";
-$mes = "Имя: $name \nE-mail: $email \nТема: $sub \nТекст: $body";
-$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
-if ($send == 'true')
-{echo "Сообщение отправлено успешно, через 6 секунд Вы будете направлены на главную страницу блога <a href='http://bloggood.ru/'>BLOGGOOD.RU</a>,где сможете продолжить ваш просмотр";}
-else {echo "Ошибка, сообщение не отправлено!";}
- 
+if(isset($name) && isset($email) && isset($sub) && isset($body))
+{
+	if(!check_recaptcha())
+	{
+		echo "Защита от ботов не пройдена!";
+		die();
+	}
+
+	$address = "info-cr-group@yandex.ru";
+	$mes = "Имя: $name \nE-mail: $email \nТема: $sub \nТекст: $body";
+	$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+	if($send == 'true')
+	{
+		echo "Сообщение отправлено успешно, через 3 секунды Вы будете направлены на главную страницу";
+	}
+	else
+	{
+		echo "Ошибка, сообщение не отправлено!";
+	}
 }
 else
 {
-echo "Вы заполнили не все поля, вернитесь назад и заполните необходимые поля!";
+	echo "Вы заполнили не все поля, вернитесь назад и заполните необходимые поля!";
 }
 ?>
